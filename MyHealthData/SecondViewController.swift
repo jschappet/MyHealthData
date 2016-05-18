bd0354ab8c7dfaadc1f9d2ddaccb6913211b8ae7
Schappet, James C <james-schappet@uiowa.edu>
2016-05-18 12:49:32 -0500

    Initial Commit

---
+++ MyHealthData/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyHealthData
+//
+//  Created by Schappet, James C on 5/18/16.
+//  Copyright © 2016 University of Iowa - ICTS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
